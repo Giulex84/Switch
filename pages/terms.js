f37a96bb25d750e7f2586045532815b128855781
Giulex84 <giulianom.gm@gmail.com>
2025-06-16 20:08:43 +0200

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,26 @@
+export default function Terms() {
+  return (
+    <main style={{ maxWidth: 600, margin: 'auto', padding: 20, fontFamily: 'Arial, sans-serif' }}>
+      <h1>Terms of Service</h1>
+      <p>
+        Utilizzando SwitchPi, accetti di rispettare questi Termini di Servizio.<br/><br/>
+        
+        SwitchPi consente agli utenti di svolgere multitask per guadagnare ricompense in Pi Network.<br/>
+        La piattaforma può modificare task e modalità di ricompensa in qualsiasi momento.<br/><br/>
+        
+        L’utente è responsabile di mantenere segrete le proprie credenziali.<br/>
+        SwitchPi non è responsabile per perdite dovute a accessi non autorizzati.<br/><br/>
+        
+        Non garantiamo l’ininterrotta disponibilità del servizio.<br/>
+        Non siamo responsabili per danni diretti o indiretti derivanti dall’uso.<br/><br/>
+        
+        La raccolta dati è regolata dalla nostra Privacy Policy.<br/><br/>
+        
+        Ci riserviamo il diritto di modificare i Termini di Servizio in qualsiasi momento.<br/><br/>
+        
+        Questi termini sono regolati dalla legge italiana.<br/>
+        Per controversie, il foro competente sarà quello di [inserire città].
+      </p>
+    </main>
+  )
+}

83edaa4b973b154a3717e6ddce2ca30b87f5b2b0
Giulex84 <giulianom.gm@gmail.com>
2025-06-16 20:09:23 +0200

    Create policy.js

---
+++ pages/policy.js
@@ -0,0 +1,20 @@
+export default function Policy() {
+  return (
+    <main style={{ maxWidth: 600, margin: 'auto', padding: 20, fontFamily: 'Arial, sans-serif' }}>
+      <h1>URL Policy</h1>
+      <p>
+        L’URL di SwitchPi è fornito per consentire agli utenti di accedere alla nostra DApp e ai servizi associati.<br/>
+        L’accesso è riservato agli utenti registrati o autorizzati.<br/>
+        È vietato utilizzare l’URL per scopi non autorizzati o illeciti.<br/><br/>
+        
+        È vietato tentare di compromettere la sicurezza del sito.<br/>
+        Non è consentito l’uso di software automatizzato senza autorizzazione.<br/><br/>
+        
+        L’URL non deve essere condiviso pubblicamente senza autorizzazione.<br/>
+        Ci riserviamo il diritto di sospendere l’accesso in caso di violazioni.<br/><br/>
+        
+        La presente policy può essere modificata in qualsiasi momento e le modifiche saranno efficaci immediatamente.
+      </p>
+    </main>
+  )
+}

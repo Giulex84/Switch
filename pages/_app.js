046b6a7354ded1a36765b7b129a0832d347dd83e
Giulex84 <giulianom.gm@gmail.com>
2025-06-17 01:44:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,18 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+import { useEffect } from 'react';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const script = document.createElement('script');
+      script.src = 'https://sdk.minepi.com/pi-sdk.js';
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}

c6379ea72d09bf168d0d72be1bacba2a2684c16f
Giulex84 <giulianom.gm@gmail.com>
2025-06-17 09:43:37 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,3 @@
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 import { useEffect } from 'react';
 import '../styles/globals.css';
 

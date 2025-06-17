7b5206a059602dc1fabc0d4cd605505046c79408
Giulex84 <giulianom.gm@gmail.com>
2025-06-17 14:54:09 +0200

    Create footer.js

---
+++ Components/footer.js
@@ -0,0 +1,11 @@
+// components/Footer.js
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '2rem 1rem', borderTop: '1px solid #eaeaea' }}>
+      <p>
+        <a href="/privacy" style={{ marginRight: '1rem' }}>Privacy Policy</a>
+        <a href="/terms">Termini di Servizio</a>
+      </p>
+    </footer>
+  );
+}

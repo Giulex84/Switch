2683d51072c21e66d42f38155edfa3dff71a59e8
Giulex84 <giulianom.gm@gmail.com>
2025-06-17 14:56:29 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import '../styles/globals.css';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -11,5 +12,10 @@ export default function App({ Component, pageProps }) {
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
